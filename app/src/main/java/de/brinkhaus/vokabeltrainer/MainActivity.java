35c57492c40b06b80586f6cfb87e457b92e852e1
Das Imperium <annika.brinkhaus@gmx.de>
2023-06-24 15:21:43 +0200

    Aufräumen der Setup Methode

--- app/src/main/java/de/brinkhaus/vokabeltrainer/MainActivity.java
+++ app/src/main/java/de/brinkhaus/vokabeltrainer/MainActivity.java
@@ -8,20 +8,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toolbar;
+
 import androidx.appcompat.app.AppCompatActivity;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Main activity of the app.
  */
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> allVocabs = new ArrayList<>();
+
 
     /**
      * This method is called when the activity is first created.
@@ -37,16 +39,45 @@ public class MainActivity extends AppCompatActivity {
         Button new_file = (Button) findViewById(R.id.new_file);
         ImageView picture = (ImageView) findViewById(R.id.picture_start);
 
-        /**
-         * Check if files exist, if not create them
-         */
+        setupVocabs();
 
+        /**
+         * This method directs the user to the trainer.
+         */
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(MainActivity.this, ViewInformation.class);
+                startActivity(a);
+            }
+        });
+    }
+
+
+
+
+    /**
+     * Check if files exist, if not create them
+     */
+
+    private void setupVocabs(){
         File datadirectory = Environment.getExternalStoragePublicDirectory("/VocTrainer/");
         File datafile = new File(datadirectory, "vocabulary.csv");
         Log.i("Vocabeltrainer: " , datafile.getAbsolutePath());
 
         if (datafile.exists()){
             Log.i("Vocabeltrainer: " , "File existiert");
+            try {
+                FileInputStream fileInputStream = new FileInputStream(datafile);
+                Scanner scanner = new Scanner(fileInputStream);
+
+                while(scanner.hasNextLine()){
+                    allVocabs.add(scanner.nextLine());
+                }
+
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
         }
         else {
             //Create Folder and Example File
@@ -60,16 +91,5 @@ public class MainActivity extends AppCompatActivity {
                 throw new RuntimeException(e);
             }
         }
-
-        /**
-         * This method directs the user to the trainer.
-         */
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent a = new Intent(MainActivity.this, ViewInformation.class);
-                startActivity(a);
-            }
-        });
     }
 }
